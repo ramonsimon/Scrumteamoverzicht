2a269549b7366a8a1bf1ee7c05f704f03eea44dc
Supermel4 <melvincuperus02@gmail.com>
2021-11-01 16:04:06 +0100

    Groepen aanmaken

--- aanmakengroep.php
+++ aanmakengroep.php
@@ -1,64 +1,64 @@
+<?php
+
+// Pathprefix
+$pathprefix = '../../';
+
+// request gebruikers
+include_once('groepen_functies.php');
+
+// Start session
+session_start();
+
+// Requests users
+$groep = new Groep();
+$groep->groepenOphalen();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="src/tailwind.css" rel="stylesheet">
-	<link href="assets/fontawesome/css/fontawesome.css" rel="stylesheet">
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css">
+	<link href="src/tailwind.css" rel="stylesheet">
+	<link rel="stylesheet" href="//use.fontawesome.com/releases/v5.0.7/css/all.css">
 	<link href="assets/fontawesome/css/solid.css" rel="stylesheet">
 	<script type="text/javascript" src="assets/navbar.js"></script>
     <?php include 'navbarvoorbeeld.php' ?>
 </head>
+
 <body>
-
-
 <div class="hidden sm:block" aria-hidden="true">
   <div class="py-5">
     <div class="border-t border-gray-200"></div>
   </div>
 </div>
 
-<div class="mt-10 sm:mt-0">
-  <div class="md:grid md:grid-cols-3 md:gap-6">
-    <div class="md:col-span-1"> </div>
-    <div class="mt-5 md:mt-0 md:col-span-2">
-      <form action="#" method="POST">
-        <div class="shadow overflow-hidden sm:rounded-md">
-          <div class="px-4 py-5 bg-white sm:p-6">
-            <div class="grid grid-cols-6 gap-6">
-              <div class="col-span-6 sm:col-span-3">
-                <label for="groepnaam" class="block text-sm font-medium text-gray-700">Groepnaam</label>
-                <input type="text" name="groepnaam" id="groepnaam" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-              </div>
-
-              <div class="col-span-6 sm:col-span-3">
-                <label for="projectnaam" class="block text-sm font-medium text-gray-700">Project naam</label>
-                <input type="text" name="projectnaam" id="projectnaam" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-              </div>
-
-              <div class="col-span-6 sm:col-span-4">
-                <label for="locatie" class="block text-sm font-medium text-gray-700">Locatie</label>
-                <input type="text" name="locatie" id="locatie" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md">
-              </div>
-
-              <div class="col-span-6 sm:col-span-3">
-                <label for="leden" class="block text-sm font-medium text-gray-700">Leden</label>
-                <select id="leden" name="leden" class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                  <option></option>
-               
-                </select>
-              </div>
+<div class="pagewrapper">
+        <div class="flex flex-wrap mb-6">
+            <div class="h-10 flex-0 sm:flex-initial mr-6"><a class="btn-primary" href="groepen.php"><i class="fas fa-arrow-left"></i> Terug</a></div>
+			<div class="h-10 leading-10 flex-0 sm:flex-grow mt-4 sm:mt-0"><h1 class="leading-6">Groep aanmaken</h1></div>
+		</div>
+    <form action="groepInsert.php" method="post">
+            <div class="flex flex-wrap my-4">
+                <div class="flex-inherit w-60"><label class="font-semibold leading-10">Groepnaam:</label></div>
+                <div class="flex-grow"><input type="text" name="groepnaam" class="input" required /></div>
             </div>
-          </div>
-          <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-            <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-              Save
-            </button>
-          </div>
-        </div>
-      </form>
+            <div class="flex flex-wrap my-4">
+                <div class="flex-inherit w-60"><label class="font-semibold leading-10" leading-10>Leden:</label></div>
+                <div class="flex-grow"><input type="text" name="leden" class="input" required /></div>
+            </div>
+            <div class="flex flex-wrap my-4">
+                <div class="flex-inherit w-60"><label class="font-semibold leading-10">Locatie:</label></div>
+                <div class="flex-grow"><input type="text" name="locatie" class="input" required /></div>
+            </div>
+            <div class="flex flex-wrap my-4">
+                <div class="flex-inherit w-60"><label class="font-semibold leading-10">Project:</label></div>
+                <div class="flex-grow"><input type="text" name="project" class="input" required /></div>
+            </div>
+            <input type="submit" name="submit" value="Toevoegen" class="btn-success cursor-pointer mt-4">
+        </form>
     </div>
   </div>
 </div>
---
+++ groepInsert.php
@@ -0,0 +1,26 @@
+<?php
+
+// Pathprefix
+$pathprefix = '../../';
+
+// Requests users
+// $gebruikers = new Gebruiker();
+// $gebruikers->gebruikersOphalen();
+
+// Includes user class
+include_once('groepen_functies.php');
+$groep = new Groep();
+
+// If submit is clicked add a user
+if(isset($_POST['submit']))
+{    
+    $groepnaam = $_POST['groepnaam'];
+    $leden = $_POST['leden'];
+    $locatie = $_POST['locatie'];
+    $projectnaam = $_POST['projectnaam'];
+    $groep->groepToevoegen($groepnaam, $leden, $locatie, $projectnaam);
+} else {
+    header("location:fail.php");
+    die;
+}
+?>
