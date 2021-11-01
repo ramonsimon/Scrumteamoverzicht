56f5f91e80a1c0edc7d5cb60c83bfadf417a6f03
Supermel4 <melvincuperus02@gmail.com>
2021-11-01 22:56:24 +0100

    Inlog niet af

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ session_start();
 					<div class="flex flex-col mb-2">
 						<div class="flex relative ">
 							<span class="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
-							<i class="fas fa-user"></i>
+							<i class="fas fa-user-circle"></i>
 							</span>
 							<input type="text" name="gebruikersnaam" class=" rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Gebruikersnaam" required/>
 						</div>
