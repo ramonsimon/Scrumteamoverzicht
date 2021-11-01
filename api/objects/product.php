f9786fb0fc695460291f2ab98d61f8d51415cd4f
Ramon <ramonsimon8@gmail.com>
2021-11-01 22:25:52 +0100

    Api users

--- api/objects/product.php
+++ api/objects/product.php
@@ -3,16 +3,14 @@ class Product{
 
     // database connection and table name
     private $conn;
-    private $table_name = "products";
+    private $table_name = "gebruikers";
 
     // object properties
     public $id;
-    public $name;
-    public $description;
-    public $price;
-    public $category_id;
-    public $category_name;
-    public $created;
+    public $gebruikersnaam;
+    public $voornaam;
+    public $achternaam;
+    public $rol;
 
     // constructor with $db as database connection
     public function __construct($db){
@@ -22,15 +20,7 @@ class Product{
     function read(){
 
         // select all query
-        $query = "SELECT
-                c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
-            FROM
-                " . $this->table_name . " p
-                LEFT JOIN
-                    categories c
-                        ON p.category_id = c.id
-            ORDER BY
-                p.created DESC";
+        $query = "SELECT * FROM gebruikers";
 
         // prepare query statement
         $stmt = $this->conn->prepare($query);
