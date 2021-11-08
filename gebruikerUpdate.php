bfadb0aef7f08ea5f1771d1024aa9e05478c4c7d
Ramon <ramonsimon8@gmail.com>
2021-11-08 13:38:46 +0100

    Kleur

--- gebruikerUpdate.php
+++ gebruikerUpdate.php
@@ -6,29 +6,26 @@ $pathprefix = '../../';
 // $gebruikers = new Gebruiker();
 // $gebruikers->gebruikersOphalen();
 
-// Start session
-session_start();
-
-//// Check if user should be able to access this page
-include_once('loginCheck.php');
-$check = new LoginCheck();
-$check->checkLogin(1);
-
 // Includes user class
-include_once('groepen_functies.php');
-$groep = new Groep();
+include_once('gebruiker_functies.php');
+$gebruiker = new Gebruiker();
 
 // If submit is clicked update the user
 if(isset($_POST['submit']))
 {
     $id = $_POST['id'];
-    $groepnaam = $_POST['groepnaam'];
-    // $leden = $_POST['leden'];
-    $locatie = $_POST['locatie'];
-    $projectnaam = $_POST['projectnaam'];
-    $groep->groepWijzigen($id, $groepnaam, $locatie, $projectnaam);
+    $gebruikersnaam = $_POST['gebruikersnaam'];
+    $voornaam = $_POST['voornaam'];
+    $achternaam = $_POST['achternaam'];
+    $wachtwoord = $_POST['wachtwoord'];
+    if (isset($wachtwoord) && $wachtwoord != '') {
+        // Password hash
+        $wachtwoord = password_hash($_POST['wachtwoord'], PASSWORD_DEFAULT);
+        $gebruiker->gebruikerWijzigenMetWachtwoord($id, $gebruikersnaam, $wachtwoord, $voornaam, $achternaam);
+    } else {
+        $gebruiker->gebruikerWijzigen($id, $gebruikersnaam, $voornaam, $achternaam);
+    }
 } else {
-    header("location:fail.php");
+    header("location:fail");
     die;
 }
-
