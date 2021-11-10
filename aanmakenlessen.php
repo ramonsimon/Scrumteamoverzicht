ab776b91398132415d626398927b77c19cb53b25
Supermel4 <melvincuperus02@gmail.com>
2021-11-10 11:57:39 +0100

    Ook voor aanmaken nu dropdowns

--- aanmakenlessen.php
+++ aanmakenlessen.php
@@ -57,16 +57,11 @@ $les->lessenOphalen();
                 <div class="flex-inherit w-60"><label class="font-semibold leading-10">Lesnaam:</label></div>
                 <div class="flex-grow"><input type="text" name="lesnaam" class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" required /></div>
             </div>
-            <div class="mt-1 focus:ring-black-500 focus:border-black-500 block w-full shadow-sm sm:text-sm border-black-900 rounded-md">
-                <div class="flex-inherit w-60"><label class="font-semibold leading-10" leading-10>Lokaal:</label></div>
-                <div class="flex-grow"><input type="text" name="lokaal" class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" required /></div>
-            </div>
             <div class="flex flex-wrap my-4">
-                    <div class="flex-inherit w-60"><label class="font-semibold leading-10">Locatie:</label></div>
+                    <div class="flex-inherit w-60"><label class="font-semibold leading-10">Lokaal:</label></div>
                     <div>
-                    <select name="locatie" class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
-                    <option selected="true" disabled="disabled" value="">--Selecteer locatie--</option>
-                        <option value="Tafel 1">Tafel 1</option>
+                    <select name="lokaal" class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                    <option selected="true" disabled="disabled" value="">--Selecteer locatie--</option>                        <option value="Tafel 1">Tafel 1</option>
                         <option value="Tafel 2">Tafel 2</option>
                         <option value="Tafel 3">Tafel 3</option>
                         <option value="Tafel 4">Tafel 4</option>
@@ -76,6 +71,18 @@ $les->lessenOphalen();
                         <option value="Cabine 4">Cabine 4</option>
                         <option value="C104">C104</option>
                         <option value="Kantine">Kantine</option>
+                    </select>
+                </div>
+                </div>
+                <div class="flex flex-wrap my-4">
+                    <div class="flex-inherit w-60"><label class="font-semibold leading-10">Dag:</label></div>
+                    <div>
+                    <select name="dag" class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent">
+                    <option selected="true" disabled="disabled" value="">--Selecteer locatie--</option>                        <option value="Maandag">Maandag</option>
+                        <option value="Dinsdag">Dinsdag</option>
+                        <option value="Woensdag">Woensdag</option>
+                        <option value="Donderdag">Donderdag</option>
+                        <option value="Vrijdag">Vrijdag</option>
                     </select>
                 </div>
                 </div>
