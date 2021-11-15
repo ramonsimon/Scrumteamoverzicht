665582f4f175ebb4c2c4b6905d6ac2432e91258f
marc446 <marcfolkertsma9@gmail.com>
2021-11-15 01:27:29 +0100

    betere styuling aanmaken pagina

--- aanmakenlessen.php
+++ aanmakenlessen.php
@@ -36,24 +36,19 @@ $les->lessenOphalen();
 <body>
 <br>
 <h1 class="text-3xl text-center">Les aanmaken</h1>
-<div class="hidden sm:block" aria-hidden="true">
-  <div class="py-5">
-    <div class="border-t border-gray-200"></div>
-  </div>
+<div class="pagewrapper">
+    
+<div class="px-4 py-5 bg-white sm:p-6">
+<a href="lessen.php" class="btn-primary"><button class="bg-blue-700 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded"><i class="fas fa-arrow-left"></i> Terug</button></a>
 </div>
 
-<div class="mt-5 md:mt-0 md:col-span-2">
-                        <div class="shadow overflow-hidden sm:rounded-md">
-                            <div class="px-4 py-5 bg-white sm:p-6">
-                                <div class="col-span-6 sm:col-span-3">
-                                    <div class="pagewrapper">
-                                        <div class="flex flex-wrap mb-6">
-            <div class="flex flex-wrap mb-6">
-                <div class="flex"><button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"><a href="lessen.php" class="btn-primary"><i class="fas fa-arrow-left"></i> Terug</a></button></div>
-                </div>
+<div class="px-4 py-5 bg-white sm:p-6">
+    <div class="shadow overflow-hidden sm:rounded-md">
+        <div class="px-4 py-5 bg-white sm:p-6">
+            <div class="col-span-6 sm:col-span-3">
 		</div>
     <form action="lesInsert.php" method="post">
-            <div class="mt-1 focus:ring-black-500 focus:border-black-500 block w-full shadow-sm sm:text-sm border-black-900 rounded-md">
+            <div class="flex flex-wrap my-4">
                 <div class="flex-inherit w-60"><label class="font-semibold leading-10">Lesnaam:</label></div>
                 <div class="flex-grow"><input type="text" name="lesnaam" class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" required /></div>
             </div>
@@ -86,23 +81,20 @@ $les->lessenOphalen();
                     </select>
                 </div>
                 </div>
-            <div class="mt-1 focus:ring-black-500 focus:border-black-500 block w-full shadow-sm sm:text-sm border-black-900 rounded-md">
+            <div class="flex flex-wrap my-4">
                 <div class="flex-inherit w-60"><label class="font-semibold leading-10">Starttijd:</label></div>
-                <div class="flex-grow"><input type="time" name="starttijd" class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" required /></div>
+                <div class="flex"><input type="time" name="starttijd" class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" required /></div>
             </div>
-            <div class="mt-1 focus:ring-black-500 focus:border-black-500 block w-full shadow-sm sm:text-sm border-black-900 rounded-md">
+            <div class="flex flex-wrap my-4">
                 <div class="flex-inherit w-60"><label class="font-semibold leading-10">Eindtijd:</label></div>
-                <div class="flex-grow"><input type="time" name="eindtijd" class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" required /></div>
-            </div>
-            <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                                            <button type="submit" name="submit" value="Toevoegen" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                                                Opslaan
-                                            </button>
-        </form>
-    </div>
-  </div>
-</div>
-</div>
+                <div class="flex"><input type="time" name="eindtijd" class="rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" required /></div>
+                </div>
+                                                        <button type="submit" name="submit" value="Toevoegen" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Opslaan</button>
+                                            </form>
+                                        </div>
+                                    </div>
+                                    </div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
